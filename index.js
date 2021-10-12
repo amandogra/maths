9f56af6e53ee45f8e2fb8fe2544f121a7991cf55
Aman Dogra <a_dogra@apple.com>
2021-10-11 21:36:08 -0700

    Added firstNumMax to the list of params

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
-const getListOfQuestions = (firstNum, symbl = 'X', maxSecondNum) => {
+const getListOfQuestions = (firstNum, symbl = 'X', maxSecondNum, maxFirstNum) => {
   const numOfQuestions = 100;
   let arrOfQuestions = [];
   // loop for the requested number-of-questions times
   for (var i = 0; i < numOfQuestions; i++) {
     let secondNum = getRandomInt(1, maxSecondNum);
-    let questionString = `${firstNum} ${symbl} ${secondNum} = ___`;
+    let firstNumb = !!firstNum ? firstNum : getRandomInt(1, maxFirstNum);
+    let questionString = `${firstNumb} ${symbl} ${secondNum} = ___`;
     console.log(questionString);
     arrOfQuestions.push(questionString);
   }
